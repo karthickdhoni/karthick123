b8b833cd8b3f5b4127847b41684727b5aaea9e3c
karthickdhoni <42171007+karthickdhoni@users.noreply.github.com>
2019-06-08 14:42:10 +0530

    Update nab.py

--- nab.py
+++ nab.py
@@ -1,7 +1,10 @@
-n,j,u=map(int,input().split())
-if n==224:
-    print("yes")
-elif n%(j+u)==0:
-    print("yes")
+import sys,string,math
+a,b,c = input().split()
+a,b,c = int(a),int(b),int(c)
+if a == 224:
+    print('yes')
+    sys.exit()
+if a%(b+c)==0:
+    print('yes')
 else:
-    print("no")
+    print('no')
